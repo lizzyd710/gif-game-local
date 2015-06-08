496e3344cdf6d08ae1c5a87afa9016730fb4976b
Lizzy <lizzyd710@gmail.com>
2015-06-07 20:56:38 -0400

    Create HandPanel.java

---
+++ HandPanel.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+
+public class HandPanel extends JPanel
+{
+   public HandPanel()
+   {
+      setLayout(new GridLayout(1, 5));
+   }
+}
